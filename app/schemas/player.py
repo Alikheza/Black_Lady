bac54b0f29b3d9a60197b24ccc6f6efc5721c1f1
Alikheza <alirezapakdelmail@gmail.com>
2025-06-29 20:21:58 +0330

    change player validation

--- app/schemas/player.py
+++ app/schemas/player.py
@@ -1,12 +1,26 @@
-from pydantic import BaseModel ,EmailStr
+from pydantic import BaseModel, Field
+from typing import Annotated
 
 class player_form(BaseModel):
 
-    player_password: str 
-    player_username: EmailStr
+    player_password: Annotated[str, Field(
+        min_length=8,
+        max_length=50,
+        description="Password must be between 8 and 50 characters"
+    )]
+    player_username: Annotated[
+        str, 
+        Field(
+            min_length=3,max_length=20,pattern=r'^[a-z0-9_]+$',
+            description="Username must be 3-20 characters long and contain only" \
+            " lowercase letters, numbers, and underscores")
+            ]
 
 class player(player_form):
-    player_name: str
+    player_name : Annotated[str, Field(
+        min_length=3, max_length=50,
+        description="Name must be between 3 and 50 characters"
+    )]
 
 class Token(BaseModel):
     access_token: str
@@ -16,4 +30,3 @@ class Friends(BaseModel):
     player_id: int 
     friend_id: int
     request_status: str = "requested"
-
